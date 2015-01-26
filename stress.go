d479358cff4c627242649136349ba941314d3a70
Branden Timm <btimm@energy.wisc.edu>
2015-01-26 15:21:03 -0600

    Relocated gomaxprocs call to init(), added comments

--- stress.go
+++ stress.go
@@ -10,12 +10,16 @@ import (
 
 var load int
 
-// On init, bind load to the '-load' flag, then parse the flags
 func init() {
+	// Bind load to the '-load' flag, then parse the flags
 	flag.IntVar(&load, `load`, 1, `Amount of load to generate`)
 	flag.Parse()
+
+	// Set golang's max number of processes to load
+	runtime.GOMAXPROCS(load)
 }
 
+// Simple function to spin on CPU until it's cancelled
 func spinUntilCancel(cancel chan bool) {
 	var i int64 = 0
 	for {
@@ -36,19 +40,18 @@ func main() {
 	// Create a channel to cancel running goroutines
 	cancel := make(chan bool, 1)
 
-	// Set golang's max number of processes to load
-	runtime.GOMAXPROCS(load)
-
 	fmt.Printf("Stress running with requested load: %d\n", load)
-	fmt.Println("Press ctrl-c at any time to exit.")
+	fmt.Println(`Press ctrl-c at any time to exit.`)
 
+	// Launch spinUntilCancel goroutines equal to requested load
 	for i := 0; i < load; i++ {
 		go spinUntilCancel(cancel)
 	}
 
+	// Wait for SIGINT or SIGKILL
 	select {
 	case s := <-sig:
-		fmt.Printf("Caught signal %s, exiting.", s)
+		fmt.Printf(`Caught signal %s, exiting.`, s)
 		cancel <- true
 	}
 	os.Exit(0)
